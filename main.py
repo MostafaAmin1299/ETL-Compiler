80196839dae0d56004d928e315a41798bf0befac
Mostafa Amin <mostafa.amin.gaber@gmail.com>
2021-10-21 17:01:33 +0200

    create project

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+class ETL:
+
+    @staticmethod
+    def read_csv():
+        pass
+
+    @staticmethod
+    def create_random_csv():
+        pass
