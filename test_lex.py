12324ff86ffea3c0e5ca67297c7b985fbfcc4f71
Abdo Mosa <abdomosa598@gmail.com>
2021-10-30 17:02:37 +0200

    customizing lex to work with sql select

--- test_lex.py
+++ test_lex.py
@@ -1,57 +1,54 @@
-# ------------------------------------------------------------
-# calclex.py
-#
-# tokenizer for a simple expression evaluator for
-# numbers and +,-,*,/
-# ------------------------------------------------------------
 import ply.lex as lex
 from ply.lex import TOKEN
 
 class MyLexer(object):
-
     # To handle reserved words
     reserved = {
-    'if' : 'IF',
-    'then' : 'THEN',
-    'else' : 'ELSE',
-    'while' : 'WHILE',
-    'for' : 'FOR',
-    'print' : 'PRINT',
+    'select':   'SELECT',
+    'from':     'FROM',
+    'into':     'INTO',
+    'where':    'WHERE',
     }
-
 
     # List of token names.   This is always required
     tokens = [
-    'NUMBER',
-    'PLUS',
-    'MINUS',
-    'TIMES',
-    'DIVIDE',
-    'LPAREN',
-    'RPAREN',
-    'NAME',
+        'NUMBER',
+        'PLUS',
+        'MINUS',
+        'TIMES',
+        'DIVIDE',
+        'LPAREN',
+        'RPAREN',
+        'NAME',
+        'EQUAL',
+        'BIGGER_THAN_OR_EQUAL_TO',
+        'BIGGER_THAN',
+        'SMALLER_THAN_OR_EQUAL_TO',
+        'SMALLER_THAN',
+        'SIMICOLON',
     ] + list(reserved.values())
 
     # Regular expression rules for simple tokens
-    t_PLUS    = r'\+'
-    t_MINUS   = r'-'
-    t_TIMES   = r'\*'
-    t_DIVIDE  = r'/'
-    t_LPAREN  = r'\('
-    t_RPAREN  = r'\)'
-    t_IF = r'if'
-    t_THEN = r'then'
-    t_ELSE = r'else'
-    t_WHILE = r'while'
-    t_FOR   = r'for'
-    t_PRINT = r'print'
+    t_PLUS      = r'\+'
+    t_MINUS     = r'-'
+    t_TIMES     = r'\*'
+    t_DIVIDE    = r'/'
+    t_LPAREN    = r'\('
+    t_RPAREN    = r'\)'
+    t_EQUAL                     = r'='
+    t_BIGGER_THAN_OR_EQUAL_TO   = r'>='
+    t_BIGGER_THAN               = r'>'
+    t_SMALLER_THAN_OR_EQUAL_TO  = r'<='
+    t_SMALLER_THAN              = r'<'
+    t_SELECT    = r'select'
+    t_FROM      = r'from'
+    t_INTO      = r'into'
+    t_WHERE     = r'where'
+    t_SIMICOLON = r';'
 
-
-    # A string containing ignored characters 
+    # ignored characters 
     t_ignore  = ' \t'  # Spaces and tabs
-    t_ignore_COMMENT = r'\#.*' # Comment
-
-
+    t_ignore_COMMENT = r'/\*.*\*/' # Comment
 
     # To define tokens as a series of more complex regular expression rules.
     digit            = r'([0-9])'
@@ -59,18 +56,13 @@ class MyLexer(object):
     identifier       = r'(' + nondigit + r'(' + digit + r'|' + nondigit + r')*)'
 
 
-
-    # Check for reserved words
     @TOKEN(identifier)
     def t_NAME(self,t):
         t.type = self.reserved.get(t.value,'NAME')    # Check for reserved words
         return t
 
-
-
-    # A regular expression rule with some action code
+    @TOKEN(r'\d+')
     def t_NUMBER(self,t):
-        r'\d+'
         t.value = int(t.value)
         return t
 
@@ -79,36 +71,30 @@ class MyLexer(object):
         r'\n+'
         t.lexer.lineno += len(t.value)
 
-
-
     # Error handling rule
     def t_error(self,t):
-        print("Illegal character '%s'" % t.value[0])
+        print(f"Illegal entity {t.value}")
         t.lexer.skip(1)
 
-
-    # Build the lexer
-    def build(self,**kwargs):
+    def __init__(self,**kwargs):
         self.lexer = lex.lex(module=self, **kwargs)
 
-    # Test it output
-    def test(self,data):
+    def tokenize(self,data):
         self.lexer.input(data)
+        print('=======Tokens=======')
         while True:
-             tok = self.lexer.token()
-             if not tok:
-                 break
-             print(tok.type, tok.value, tok.lineno, tok.lexpos)
-
-
+            tok = self.lexer.token()
+            if not tok:
+                break
+            print("\t", tok.value, "\t:\t", tok.type, sep="")
+        print('====================')
 
 
 # Build the lexer and try it out
 m = MyLexer()
-m.build()           # Build the lexer
-m.test(
-    '''
-    x = 2 + 4 * 7
-    # hello
-    print(aa)
-    ''')     # Test it
+
+while(True):
+    q = input("Enter Query (q to exit): ")
+    if q == 'q':
+        break
+    m.tokenize(q)
