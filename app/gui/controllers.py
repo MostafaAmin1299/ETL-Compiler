9cab8831cbbd4a3d4ee5a5315f87391284a44a81
Abdo Mosa <abdomosa598@gmail.com>
2021-12-26 19:04:19 +0200

    stable version

--- app/gui/controllers.py
+++ app/gui/controllers.py
@@ -8,18 +8,18 @@ from tabulate import tabulate
 
 
 def compile():
-    try:
+    # try:
         query = str(ui.inputbox.text())
         query = query.lower()
         result = parser.parse(query)
         ui.outputbox.setText(str(result))
-    except Exception as e:
-        print("Compilation Error.", e)
+    # except Exception as e:
+    #     print("Compilation Error.", e)
 
 
 
 def execute():
-    try:
+    # try:
         current_time = datetime.now().strftime("%H:%M:%S")
         start_time = time.time()
         code = str(ui.outputbox.toPlainText())
@@ -29,11 +29,11 @@ def execute():
         )
 
         from app.etl import core
-        if type(core.result) == str:
+        if isinstance(core.result, str):
             ui.results.setText(
                 ui.results.toPlainText() + f"\n{core.result}\n"
             )
-        elif core.result != None:
+        else:
             table = tabulate(core.result, headers=core.result.keys())
             ui.results.setText(
                 ui.results.toPlainText() + f"\n{table}\n"
@@ -42,5 +42,5 @@ def execute():
         ui.results.setText(
             ui.results.toPlainText() + f"\nExcecution process took: {(time.time() - start_time) * pow(10, 3):.2f} Milisecond\n")
 
-    except Exception as e:
-        print("Execution Error.", e)
+    # except Exception as e:
+    #     print("Execution Error.", e)
